4dd16e15eaabd599253646e35f088e565c8868d8
sagarprakash055 <sagarprakash108@gmail.com>
2026-06-29 14:28:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (33.74%) - LeetSync

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int n = patterns.size();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (word.contains(patterns[i])) {
+                count++;
+            }else{
+                continue;
+            }
+        }
+        return count;
+    }
+};
