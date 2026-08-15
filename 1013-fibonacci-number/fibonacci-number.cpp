557bc9dd541f60b705dacb9a622e7b388b74ba28
sagarprakash055 <sagarprakash108@gmail.com>
2026-08-15 23:43:43 +0530

    Time: 2 ms (77.62%) | Memory: 7.7 MB (89.00%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int dp[100];
+    int fib(int n) {
+        if(n <= 1) return n;
+        if(dp[n] != 0) return dp[n];
+        dp[n] = fib(n-1) + fib(n-2);
+        return dp[n];
+    }
+};
