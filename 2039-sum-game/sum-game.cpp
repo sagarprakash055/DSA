25d23f0d7dc27c530703da82d2c4fb9232a0c95d
sagarprakash055 <sagarprakash108@gmail.com>
2026-08-24 15:50:01 +0530

    Time: 7 ms (21.62%) | Memory: 14 MB (67.70%) - LeetSync

--- 2039-sum-game/sum-game.cpp
+++ 2039-sum-game/sum-game.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     bool sumGame(string num) {
-        int n = num.length();
+        int n = num.size();
         int sumL = 0, sumR = 0;
         int qL = 0, qR = 0;
 
-        // Count sums and '?' for the left half
-        for (int i = 0; i < n / 2; i++) {
-            if (num[i] == '?') qL++;
+        for(int i = 0; i < n/2; i++){
+            if(num[i] == '?') qL++;
             else sumL += num[i] - '0';
         }
-        
-        // Count sums and '?' for the right half
-        for (int i = n / 2; i < n; i++) {
-            if (num[i] == '?') qR++;
+
+        for(int i = n/2; i < n; i++){
+            if(num[i] == '?') qR++;
             else sumR += num[i] - '0';
         }
 
-        // Alice wins if the total number of '?' is odd
-        if ((qL + qR) % 2 != 0) {
-            return true; 
+        if((qL + qR) % 2 != 0){
+            return true;
         }
 
-        // If even, Alice wins ONLY IF Bob cannot perfectly balance the board
-        return 2 * (sumL - sumR) != 9 * (qR - qL);
+        return 2*(sumL - sumR) != 9*(qR - qL);
     }
 };
